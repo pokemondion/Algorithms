cf2521fee4aae79e902bba93abc6f16f67f7a582
Dion Tu <diontu1@gmail.com>
2019-11-24 12:17:21 -0500

    merge sort implementation and test

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -2,4 +2,56 @@ package sorting;
 
 public class MergeSort {
 
+	// this merge sort keeps track of the indices of the array
+	public static void mergeSort(int[] arr, int low, int high) {
+		if (low < high) {
+			int mid = (low + high)/2;
+			mergeSort(arr, low, mid);
+			mergeSort(arr, mid + 1, high);
+			merge(arr, low, mid, high);
+		}
+	}
+	
+	private static void merge(int[] arr, int low, int mid, int high) {
+		// get the two subarrays to be merged
+		int size1 = mid - low + 1;
+		int size2 = high - mid;
+		int[] subarr1 = new int[size1];
+		int[] subarr2 = new int[size2];
+		
+		//copy data to temp arrays
+		//subarr1
+		for (int i = 0; i < size1; i ++) {
+			subarr1[i] = arr[low + i];
+		}
+		//subarr2
+		for (int i = 0; i < size2; i ++) {
+			subarr2[i] = arr[mid + 1 + i];
+		}
+		
+		// i = index for first array, j = index for second array
+		int i = 0, j = 0;
+		int arrIndex = low;
+		while (i < size1 && j < size2) {
+			if (subarr1[i] > subarr2[j]) {
+				arr[arrIndex] = subarr2[j];
+				j ++;
+			}
+			else {
+				arr[arrIndex] = subarr1[i];
+				i ++;
+			}
+			arrIndex ++;
+		}
+		while (i < size1) {
+			arr[arrIndex] = subarr1[i];
+			i ++;
+			arrIndex ++;
+		}
+		while (j < size2) {
+			arr[arrIndex] = subarr2[j];
+			j ++;
+			arrIndex ++;
+		}
+	}
 }
---
+++ src/test/java/sorting/MergeSortTest.java
@@ -0,0 +1,24 @@
+package sorting;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MergeSortTest {
+	
+	@Test
+	public void testMergeSort() {
+		int[] testArray = {21,61,34,85,11,24,22,21};
+		int[] expectedArray = {11,21,21,22,24,34,61,85};
+		int size = testArray.length;
+		
+		MergeSort.mergeSort(testArray, 0, size-1);
+		boolean correct = true;
+		for (int i = 0; i < size; i++) {
+			if (testArray[i] != expectedArray[i]) {
+				correct = false;
+			}
+		}
+		assertTrue("Merge Sort was not implemented correctly",correct);
+	}
+
+}
