46746450b74ed253cf4116fa595f74abc2802ffc
pokemondion <diontu1@gmail.com>
2019-12-10 15:17:30 -0500

    heapsort

--- src/main/java/sorting/HeapSort.java
+++ src/main/java/sorting/HeapSort.java
@@ -3,7 +3,22 @@ package sorting;
 public class HeapSort {
 	
 	public void HeapingSort(int[] array) {
+		// leftchild from parent = 2n
+		// rightchild from parent = 2n + 1
+		int size = array.length;
+		for (int i = size; i > 0; i --) {
+			
+		}
+	}
+	
+	private void constructMaxHeap(int[] array) {
 		
+	}
+	
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 	
 }
