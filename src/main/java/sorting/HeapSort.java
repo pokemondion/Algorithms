a7e9f71aa46dd8542728a9da5bcc1e32e15b8fbf
pokemondion <diontu1@gmail.com>
2019-12-10 18:17:27 -0500

    heapsort1

--- src/main/java/sorting/HeapSort.java
+++ src/main/java/sorting/HeapSort.java
@@ -1,24 +1,73 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class HeapSort {
 	
-	public void HeapingSort(int[] array) {
-		// leftchild from parent = 2n
-		// rightchild from parent = 2n + 1
+	public static void HeapHippityHopSort(int[] array) {
 		int size = array.length;
+
 		for (int i = size; i > 0; i --) {
+			
+			constructMaxHeap(array);
+		//	swap(0, i-1, array);
 			
 		}
 	}
 	
-	private void constructMaxHeap(int[] array) {
-		
+	private static int rightChildPos(int pos) {
+		return (pos * 2) + 1;
 	}
 	
-	private void swap(int[] array, int i, int j) {
+	private static int leftChildPos(int pos) {
+		return (pos * 2);
+	}
+	
+	private static boolean isLeaf(int pos, int[] array) {
+		int size = array.length;
+		if (pos >= (size/2) + 1) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static void constructMaxHeap(int[] array) {
+		int size = array.length;
+		for (int i = size/2; i >= 0; i --) {
+			downMaxHeap(i, array);
+		}
+	}
+	
+	private static void downMaxHeap(int index, int[] array) {
+		int pos = index + 1;
+		int size = array.length;
+		if (!isLeaf(pos, array)) {
+			if (array[leftChildPos(pos) - 1] > array[index] || array[rightChildPos(pos) - 1] > array[index]) {
+				if (array[leftChildPos(pos) - 1] > array[rightChildPos(pos) - 1]) {
+					swap(index, leftChildPos(pos) - 1, array);
+					downMaxHeap(leftChildPos(pos) - 1, array);
+				}
+				else {
+					swap(index, rightChildPos(pos) - 1, array);
+					downMaxHeap(rightChildPos(pos) - 1, array);
+				}
+			}
+		}
+	}
+	
+	private static void swap(int i, int j, int[] array) {
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
+	}
+	
+	public static void main (String[] args) {
+		int[] test = {7,3,5,6,8};
+		int[] expted = {8,7,5,6,3};
+		HeapSort.HeapHippityHopSort(test);
+		for (int i = 0; i < test.length; i ++) {
+			System.out.print(test[i] + " ");
+		}
 	}
 	
 }
